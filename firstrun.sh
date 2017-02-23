fd756df2d632088d865dae6aa636fe1a3f860feb
Andrew Hilts <andrewchilts@gmail.com>
2017-02-23 11:17:45 -0500

    fix AMI CMS role

--- firstrun.sh
+++ firstrun.sh
@@ -4,6 +4,9 @@ vagrant plugin install vagrant-hostsupdater
 vagrant plugin install vagrant-vbguest
 echo "Creating shared folder `ami-code` for development"
 mkdir ami-code
+mkdir roles/amicms-files/files
+mkdir roles/amicms-db/files
+mkdir roles/ami-community-db/files
 cd roles/wordpress-host/files/cert
 echo "Creating SSL parameters and certificates"
 bash generate_self_signed_cert.sh
